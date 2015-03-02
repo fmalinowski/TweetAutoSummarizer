2b461c8bb47bad75b661ae3e08184efd7cd8a614
myb800 <myb800@gmail.com>
2015-03-01 20:37:02 -0800

    new link cleaner

--- apps/Tweet-Cleaner/src/Tweet_Cleaner/LinkCleaner.java
+++ apps/Tweet-Cleaner/src/Tweet_Cleaner/LinkCleaner.java
@@ -9,14 +9,16 @@ import java.io.PrintWriter;
 
 
 public class LinkCleaner {
-	private static final String LINK_REGEX = "http://t\\.co/\\w*";
+	private static final String LINK_REGEX = "(\\()*http(s)*://t\\.co/\\w*(\\))*(( )*бн)*|http://(t\\.)*\\w*( )*бн|http(:)*( )*бн|http://( )*бн";
+	private static final String LINK_REGEX2 = "http://(t)*";
+	private static final String LINK_REGEX3 = "http(:)*|//";
 	public static void cleanLink(File in,File out){
 		try {
 			BufferedReader bReader = new BufferedReader(new FileReader(in));
 			PrintWriter pWriter = new PrintWriter(out);
 			String line = bReader.readLine();
 			while(line != null){
-				line = line.replaceAll(LINK_REGEX, "");
+				line = line.replaceAll(LINK_REGEX, "").replaceAll(LINK_REGEX2, "").replaceAll(LINK_REGEX3, "");
 				pWriter.write(line);
 				pWriter.write("\n");
 				line = bReader.readLine();
